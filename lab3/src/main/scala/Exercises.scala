4e772398b0f9ab06fd1e71b31d34ec259b1afa3b
Martin Schoeberl <martin@jopdesign.com>
2025-08-14 13:33:58 +0200

    Start lab2 and lab3

---
+++ lab3/src/main/scala/Exercises.scala
@@ -0,0 +1,16 @@
+object Exercises {
+  // 1. Double Elements
+  def doubleElements(lst: List[Int]): List[Int] = {
+    ???
+  }
+
+  // 2. Filter Odd Numbers
+  def filterOddNumbers(lst: List[Int]): List[Int] = {
+    ???
+  }
+
+  // 3. Higher-Order Function
+  def applyFunction[A, B](f: A => B, lst: List[A]): List[B] = {
+    ???
+  }
+}
