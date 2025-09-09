a7691b94007afcb53f1ebc08a42b877cbbd47ebb
Martin Schoeberl <martin@jopdesign.com>
2025-09-09 09:19:41 +0200

    lab2: drop MaxFindeTest code

--- lab2/src/test/scala/MaxFinderTest.scala
+++ lab2/src/test/scala/MaxFinderTest.scala
@@ -6,18 +6,7 @@ import org.scalatest.matchers.should.Matchers
 class MaxFinderTest extends AnyFlatSpec with ChiselScalatestTester with Matchers {
   "MaxFinder" should "find the maximum value in a Vec" in {
     test(new MaxFinder(4, 8)) { dut =>
-      val testVectors = Seq(
-        (Seq(3, 7, 2, 5), 7),
-        (Seq(10, 4, 8, 1), 10),
-        (Seq(0, 0, 0, 0), 0),
-        (Seq(1, 2, 3, 4), 4)
-      )
-      for ((vec, expected) <- testVectors) {
-        for (i <- vec.indices) {
-          dut.io.in(i).poke(vec(i).U)
-        }
-        dut.io.max.expect(expected.U)
-      }
+      ???
     }
   }
 }
